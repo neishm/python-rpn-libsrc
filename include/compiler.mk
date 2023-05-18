99975e05ed3e679411d8b8e2b23608967fb10e95
Mike Neish <Michael.Neish@ec.gc.ca>
2023-05-18 10:21:24 -0400

    Fix compatiblity issues with GCC 10.
    Getting errors about type mismatches, something about "boz" constants, and
    duplicate definitions of global variables.  There's some new flags to work
    around these issues.

--- include/compiler.mk
+++ include/compiler.mk
@@ -7,5 +7,10 @@ endif
 # Extra flags required for gfortran.
 ifneq (,$(findstring gfortran,$(FC)))
 FFLAGS := $(FFLAGS) -fcray-pointer -ffree-line-length-none
+GCC10CHECK=$(shell gcc -dumpversion | cut -c2)
+ifneq (,$(GCC10CHECK))
+FFLAGS := $(FFLAGS) -fallow-argument-mismatch -fallow-invalid-boz
+CFLAGS := $(CFLAGS) -fcommon
+endif
 endif
 
